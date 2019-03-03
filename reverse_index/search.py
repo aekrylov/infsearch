882ca748094c2e559a3a2ee08e0e2a9cd907455b
Anton Krylov <anthony.kryloff@gmail.com>
2019-03-03 19:26:03 +0300

    add reverse index search

---
+++ reverse_index/search.py
@@ -0,0 +1,28 @@
+from peewee import fn
+
+from models import Article, Stem, StemArticle, init_db
+from stemming import tokenize, stem_porter
+
+
+def find(query):
+    tokens = [stem_porter(token) for token in tokenize(query)]
+
+    count = fn.SUM(StemArticle.count).alias('rank')
+    count_distinct = fn.COUNT(StemArticle.stem.distinct())
+
+    articles = (Article.select(Article, count, count_distinct)
+                .join(StemArticle)
+                .join(Stem)
+                .where(Stem.term.in_(tokens) & (Stem.type == 'porter'))
+                .group_by(Article.id)
+                .having(count_distinct == len(tokens))
+                .order_by(count.desc())
+                )
+
+    return articles
+
+
+if __name__ == '__main__':
+    init_db()
+    for article in find('Украина'):
+        print('%3d; %s' % (article.rank, article.title))
